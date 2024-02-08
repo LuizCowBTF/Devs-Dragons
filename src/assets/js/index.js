46bd1755265e58da23b9aa5d034cd8d6c3dca4a4
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-08 13:42:32 -0300

    module arqueiro-mago

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,10 +1,14 @@
 import { Personagem } from "../../modules/personagem.js";
 import { PersonagemView } from "../../components/personagem-view.js";
 import { Mago } from "../../modules/mago.js";
+import { Arqueiro } from "../../modules/arqueiro.js";
+import { ArqueiroMago } from './../../modules/arqueiro-mago.js';
 
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3, '')
 const magaPatalogica = new Mago('Patalógica', 8, 'gelo', 7, 10, '')
+const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magaPatalogica]
+const personagens = [magoAntonio, magaPatalogica, arqueiroBruno, arqueiroMagoChico]
 
 new PersonagemView(personagens).render()
